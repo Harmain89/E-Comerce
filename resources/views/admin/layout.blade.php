1211ef734c03093754847252cab280feaac68d88
Harmain89 <88616082+Harmain89@users.noreply.github.com>
2022-10-28 10:58:42 +0500

    Update layout.blade.php

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -457,7 +457,7 @@ use Illuminate\Support\Facades\URL;
                 </div>
 
             <!-- Modal -->
-            <div class="modal fade-in" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal fade-out" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
